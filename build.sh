83a28513d6480cf3734d59d2ea69866434618a51
Roler <roler.huaman@gmail.com>
2023-07-03 23:36:43 -0500

    test

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git init
+git pull https://github.com/rolhuama/pruebasdecodigo.git
+gradle bootjar
+docker build -t demo .
+docker tag demo rolervengador/demo
+docker push rolervengador/demo
+docker run -dp 8080:8080 demo
