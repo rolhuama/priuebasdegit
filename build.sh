0dbbab5ae72439a5c109a42b4e06950af31aea6e
rolhuama <127351281+rolhuama@users.noreply.github.com>
2023-07-06 12:22:54 -0500

    ..

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git init
-git pull https://github.com/rolhuama/pruebasdecodigo.git
-gradle bootjar
-docker build -t demo .
-docker tag demo rolervengador/demo
-docker push rolervengador/demo
-docker run -dp 9000:8080 demo
