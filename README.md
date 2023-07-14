2e268027fd0d85b4d0d106c4df63258beeb615dc
Roler <roler.huaman@gmail.com>
2023-07-14 10:37:55 -0500

    aa

--- README.md
+++
@@ -1,3 +0,0 @@
-# aplicacion
-# aplicacion
-# aplicacion
--- kubernetes_deployment.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
-sleep 100
-kubectl port-forward deployment.apps/devops 9000:8080 &
